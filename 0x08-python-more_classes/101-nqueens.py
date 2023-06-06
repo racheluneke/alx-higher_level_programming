5e6c97a98541e73bd0bc03de542c10da84f074c5
racheluneke <rackelchi07@gmail.com>
2023-06-05 22:22:53 -0700

    a program that solves the N queens problem

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,125 +1,107 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle"""
+"""
 
-import sys
+This module contains an algorithm that resolves the N-Queen puzzle
+using backtracking
 
-
-def init_board(n):
-    """Initialize an `n`x`n` sized chessboard with 0's."""
-    board = []
-    [board.append([]) for _ in range(n)]
-    [row.append(' ') for _ in range(n) for row in board]
-    return board
+"""
 
 
-def copy_board(board):
-    """Return a deepcopy of a chessboard."""
-    if isinstance(board, list):
-        return list(map(copy_board, board))
-    return board
-
-
-def get_solution(board):
-    """Return the list of lists representation of a solved chessboard."""
-    solution = []
-    for r in range(len(board)):
-        for c in range(len(board)):
-            if board[r][c] == "Q":
-                solution.append([r, c])
-                break
-    return solution
-
-
-def mark_unavail_spots(board, row, col):
-    """Mark spots on a chessboard as unavailable.
-
-    All spots where non-attacking queens can no
-    longer be placed are marked as 'x'.
+def isSafe(m_queen, nqueen):
+    """ Method that determines if the queens can or can't kill each other
 
     Args:
-        board (list): The current working chessboard.
-        row (int): The row where a queen was last placed.
-        col (int): The column where a queen was last placed.
-    """
-    # Mark all forward spots as 'x'
-    for c in range(col + 1, len(board)):
-        board[row][c] = "x"
-    # Mark all backward spots as 'x'
-    for c in range(col - 1, -1, -1):
-        board[row][c] = "x"
-    # Mark all spots below as 'x'
-    for r in range(row + 1, len(board)):
-        board[r][col] = "x"
-    # Mark all spots above as 'x'
-    for r in range(row - 1, -1, -1):
-        board[r][col] = "x"
-    # Mark all spots diagonally down to the right as 'x'
-    c = col + 1
-    for r in range(row + 1, len(board)):
-        if c >= len(board):
-            break
-        board[r][c] = "x"
-        c += 1
-    # Mark all spots diagonally up to the left as 'x'
-    c = col - 1
-    for r in range(row - 1, -1, -1):
-        if c < 0:
-            break
-        board[r][c]
-        c -= 1
-    # Mark all spots diagonally up to the right as 'x'
-    c = col + 1
-    for r in range(row - 1, -1, -1):
-        if c >= len(board):
-            break
-        board[r][c] = "x"
-        c += 1
-    # Mark all spots diagonally down to the left as 'x'
-    c = col - 1
-    for r in range(row + 1, len(board)):
-        if c < 0:
-            break
-        board[r][c] = "x"
-        c -= 1
+        m_queen: array that has the queens positions
+        nqueen: queen number
 
-
-def solve_nqueens(board, row, queens, solu):
-    """Recursively solve an N-queens puzzle.
-
-    Args:
-        board (list): The current working chessboard.
-        row (int): The current working row.
-        queens (int): The current number of placed queens.
-        solu (list): A list of lists of solutions.
     Returns:
-        solu
+        True: when queens can't kill each other
+        False: when some of the queens can kill
+
     """
-    if queens == len(board):
-        solu.append(get_solution(board))
-        return solu
 
-    for col in range(len(board)):
-        if board[row][col] == " ":
-            t_board = copy_board(board)
-            t_board[row][col] = "Q"
-            mark_unavail_spots(t_board, row, col)
-            solu = solve_nqueens(t_board, row + 1, queens + 1, solu)
+    for i in range(nqueen):
 
-    return solu
+        if m_queen[i] == m_queen[nqueen]:
+            return False
+
+        if abs(m_queen[i] - m_queen[nqueen]) == abs(i - nqueen):
+            return False
+
+    return True
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
+def print_result(m_queen, nqueen):
+    """ Method that prints the list with the Queens positions
+
+    Args:
+        m_queen: array that has the queens positions
+        nqueen: queen number
+
+    """
+
+    res = []
+
+    for i in range(nqueen):
+        res.append([i, m_queen[i]])
+
+    print(res)
+
+
+def Queen(m_queen, nqueen):
+    """ Recursive function that executes the Backtracking algorithm
+
+    Args:
+        m_queen: array that has the queens positions
+        nqueen: queen number
+
+    """
+
+    if nqueen is len(m_queen):
+        print_result(m_queen, nqueen)
+        return
+
+    m_queen[nqueen] = -1
+
+    while((m_queen[nqueen] < len(m_queen) - 1)):
+
+        m_queen[nqueen] += 1
+
+        if isSafe(m_queen, nqueen) is True:
+
+            if nqueen is not len(m_queen):
+                Queen(m_queen, nqueen + 1)
+
+
+def solveNQueen(size):
+    """ Function that invokes the Backtracking algorithm
+
+    Args:
+        size: size of the chessboard
+
+    """
+
+    m_queen = [-1 for i in range(size)]
+
+    Queen(m_queen, 0)
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    if len(sys.argv) == 1 or len(sys.argv) > 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if sys.argv[1].isdigit() is False:
+
+    try:
+        size = int(sys.argv[1])
+    except:
         print("N must be a number")
         sys.exit(1)
-    if int(sys.argv[1]) < 4:
+
+    if size < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    chessboard = init_board(int(sys.argv[1]))
-    solution_set = solve_nqueens(chessboard, 0, 0, [])
-    for solution in solution_set:
-        print(solution)
+    solveNQueen(size)
