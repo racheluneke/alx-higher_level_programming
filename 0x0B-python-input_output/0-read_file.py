fefd4daca9f70378a454a7dbda22b3b292c12fe6
racheluneke <rackelchi07@gmail.com>
2023-06-13 18:59:16 -0700

    a function that read a test file utf8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,6 @@
+#!/usr/bin/python3
+
 def read_file(filename=""):
-  """
-  Reads a text file (UTF8) and prints it to stdout.
-
-  Args:
-    filename: The path to the text file.
-
-  Returns:
-    None.
-  """
-
-  with open(filename, "r", encoding="utf-8") as f:
-    for line in f:
-      print(line, end="")
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            print(line,end="")
