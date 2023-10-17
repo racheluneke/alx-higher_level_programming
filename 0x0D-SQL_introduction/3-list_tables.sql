8d101e3bbb9fd0388a8e46630a4cddf5e210a433
racheluneke <rackelchi07@gmail.com>
2023-10-17 10:25:52 -0700

    creating a tables of database

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- a script that lists all the tables of a database in your MySQL serve
+-- The database name will be passed as argument of mysql
+-- command (in the following example: mysql is the name of the database)
+
+SHOW TABLES;
